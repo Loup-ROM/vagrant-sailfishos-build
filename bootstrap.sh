71759167eb2e907ea8d7c1149c65f1c53a8c6b58
bitrvmpd <e.noyer.silva@gmail.com>
2018-11-02 22:27:35 -0600

    :bug: Fixing paths
    - Using /home/vagrant/sfos as our root dir.
    - Device and vendor config added again.
    - Todo: repo and git config.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/env bash
 
+# ############################### #
+# SET YOUR VENDOR AND DEVICE HERE #
+# ############################### #
+export VENDOR="xiaomi"
+export DEVICE="santoni"
+export PORT_ARCH="armv7hl"
+
 # Format second disk for persistent storage
 dev='/dev/sdc'
 sudo umount "$dev"
 printf "o\nn\np\n1\n\n\nw\n" | sudo fdisk "$dev"
 sudo mkfs.ext4 "${dev}1"
 
-mkdir /mnt/develop
-echo `blkid /dev/sdc1 | awk '{print$2}' | sed -e 's/"//g'` /mnt/develop   ext4   noatime,nobarrier   0   0 >> /etc/fstab
-mount /mnt/develop
+mkdir /home/vagrant/sfos
+echo `blkid /dev/sdc1 | awk '{print$2}' | sed -e 's/"//g'` /home/vagrant/sfos   ext4   noatime,nobarrier   0   0 >> /etc/fstab
+mount /home/vagrant/sfos
 
-chmod -R ug+wx /mnt/develop
-chown vagrant:vagrant -R /mnt/develop
-
-ln -s /mnt/develop /home/vagrant/develop
+chmod -R ug+wx /home/vagrant/sfos
+chown vagrant:vagrant -R /home/vagrant/sfos
 
 # SAILFISH-HADK
 export HOME="/home/vagrant"
-export HADK_ROOT="/mnt/develop"
+export HADK_ROOT="$HOME/sfos"
 
-cat <<'EOF' > $HOME/.hadk.env
-export HADK_ROOT="/mnt/develop" 
-export PLATFORM_SDK_ROOT="$HADK_ROOT/mer" 
-export ANDROID_ROOT="$HADK_ROOT/hadk" 
-export VENDOR="xiaomi"
-export DEVICE="santoni"
+cat <<EOF > $HOME/.hadk.env
+export HADK_ROOT="/home/vagrant/sfos" 
+export PLATFORM_SDK_ROOT="\$HADK_ROOT/mer" 
+export ANDROID_ROOT="\$HADK_ROOT/hadk" 
+export VENDOR="$VENDOR"
+export DEVICE="$DEVICE"
 # ARCH conflicts with kernel build 
-export PORT_ARCH="armv7hl"
+export PORT_ARCH="$PORT_ARCH"
 EOF
 
 cat <<'EOF' >> $HOME/.mersdkubu.profile
@@ -58,13 +63,18 @@ sudo chown vagrant:vagrant $HOME/.mersdkubu.profile
 
 # Enter to PLATFORM_SDK
 echo "##### Enter to sfossdk"
-runuser -l vagrant -c 'printf "c\nc\nsudo zypper -n in android-tools-hadk tar\nexit\n" | /mnt/develop/mer/sdks/sfossdk/mer-sdk-chroot'
+runuser -l vagrant -c "printf 'c\nc\nsudo zypper -n in android-tools-hadk tar\nexit\n' | /home/vagrant/sfos/mer/sdks/sfossdk/mer-sdk-chroot"
 
 # Installing the corresponding SDK
 echo "##### Installing SDKs"
-runuser -l vagrant -c 'printf "sdk-assistant create SailfishOS-latest http://releases.sailfishos.org/sdk/latest/Jolla-latest-Sailfish_SDK_Tooling-i486.tar.bz2\ny\nexit" | /mnt/develop/mer/sdks/sfossdk/mer-sdk-chroot'
-runuser -l vagrant -c 'printf "sdk-assistant create SailfishOS-latest-armv7hl http://releases.sailfishos.org/sdk/latest/Jolla-latest-Sailfish_SDK_Target-armv7hl.tar.bz2\ny\nexit" | /mnt/develop/mer/sdks/sfossdk/mer-sdk-chroot'
+runuser -l vagrant -c "printf 'sdk-assistant create SailfishOS-latest http://releases.sailfishos.org/sdk/latest/Jolla-latest-Sailfish_SDK_Tooling-i486.tar.bz2\ny\nexit' | /home/vagrant/sfos/mer/sdks/sfossdk/mer-sdk-chroot"
+runuser -l vagrant -c "printf 'sdk-assistant create ${VENDOR}-${DEVICE}-${PORT_ARCH} http://releases.sailfishos.org/sdk/latest/Jolla-latest-Sailfish_SDK_Target-armv7hl.tar.bz2\ny\nexit' | /home/vagrant/sfos/mer/sdks/sfossdk/mer-sdk-chroot"
 
 # Installing ubuntu chroot (Android build environment)
 echo "##### Installing Ubuntu Chroot"
-runuser -l vagrant -c 'printf "curl -s -O https://releases.sailfishos.org/ubu/ubuntu-trusty-20180613-android-rootfs.tar.bz2 && sudo mkdir -p /mnt/develop/mer/sdks/ubuntu && sudo tar --numeric-owner -xjf ubuntu-trusty-20180613-android-rootfs.tar.bz2 -C /mnt/develop/mer/sdks/ubuntu && exit" | /mnt/develop/mer/sdks/sfossdk/mer-sdk-chroot'
+runuser -l vagrant -c "printf 'curl -s -O https://releases.sailfishos.org/ubu/ubuntu-trusty-20180613-android-rootfs.tar.bz2 && sudo mkdir -p /home/vagrant/sfos/mer/sdks/ubuntu && sudo tar --numeric-owner -xjf ubuntu-trusty-20180613-android-rootfs.tar.bz2 -C /home/vagrant/sfos/mer/sdks/ubuntu && exit' | /home/vagrant/sfos/mer/sdks/sfossdk/mer-sdk-chroot"
+
+# Fixing missing keys
+
+# Installing repo utils
+
